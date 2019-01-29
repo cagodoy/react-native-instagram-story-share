e2823cfb13c83fee3d039285cab416c4b3d80c59
Camilo Acuña <camilo.aku@gmail.com>
2019-01-29 16:31:52 -0300

    Add implementation to use new attrs from instgram/facebook stories

--- ios/RNInstagramStoryShare/RNInstagramStoryShare.m
+++ ios/RNInstagramStoryShare/RNInstagramStoryShare.m
@@ -1,110 +1,156 @@
 #import "RCTBridgeModule.h"
+
 #import "RNInstagramStoryShare.h"
-// import RCTConvert
 #if __has_include(<React/RCTConvert.h>)
+
 #import <React/RCTConvert.h>
 #elif __has_include("RCTConvert.h")
+
 #import "RCTConvert.h"
 #else
-#import "React/RCTConvert.h"   // Required when used as a Pod in a Swift project
+
+#import "React/RCTConvert.h"
 #endif
 
 @implementation RNInstagramStoryShare
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(share:(NSDictionary *)options
-                  failureCallback:(RCTResponseErrorBlock)failureCallback
-                  successCallback:(RCTResponseSenderBlock)successCallback) {
-    NSString *deeplinkingUrl = [RCTConvert NSString:options[@"deeplinkingUrl"]];
-    NSURL *backgroundImageUrl = [RCTConvert NSURL:options[@"backgroundImage"]];
-    
-    if(deeplinkingUrl && backgroundImageUrl){
-        if ([backgroundImageUrl.scheme.lowercaseString isEqualToString:@"data"]) {
-            NSString *errorMessage = @"Data conversion failed";
-            NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(errorMessage, nil)};
-            NSError *error = [NSError errorWithDomain:@"com.rninstagramstoryshare" code:1 userInfo:userInfo];
-
-            /* convert base64 string with scheme to base64 string only */
-            NSData *data = [NSData dataWithContentsOfURL:backgroundImageUrl
-                                                 options:(NSDataReadingOptions)0
-                                                   error:&error];
-            
-            if (!data) {
-                failureCallback(error);
-                return;
-            }
-
-            /* deeplink to instagram */
-            [self backgroundImage:UIImagePNGRepresentation([UIImage imageWithData:data])
-                  attributionURL: deeplinkingUrl
-                  failureCallback:failureCallback
-                  successCallback:successCallback];
-
-        } else {
-            /* handle non base64 images */
-            NSString *errorMessage = @"No base64 image";
-            NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(errorMessage, nil)};
-            NSError *error = [NSError errorWithDomain:@"com.rninstagramstoryshare" code:1 userInfo:userInfo];
-
-            failureCallback(error);
-        }
-    } else {
-        /* handle non existing assets */
-        NSString *errorMessage = @"Invalid parameter";
-        NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(errorMessage, nil)};
-        NSError *error = [NSError errorWithDomain:@"com.rninstagramstoryshare" code:1 userInfo:userInfo];
-        
-        failureCallback(error);
-    }
+- (dispatch_queue_t)methodQueue {
+  return dispatch_get_main_queue();
 }
 
-- (void)backgroundImage:(NSData *)backgroundImage
-                attributionURL:(NSString *)attributionURL
-                failureCallback:(RCTResponseErrorBlock)failureCallback
-                successCallback:(RCTResponseSenderBlock)successCallback
-{
-    
-    // Verify app can open custom URL scheme, open if able
-    NSURL *urlScheme = [NSURL URLWithString:@"instagram-stories://share"];
-    if ([[UIApplication sharedApplication] canOpenURL:urlScheme]) {
-        
-        // Assign background image asset and attribution link URL to pasteboard
-        NSArray *pasteboardItems = @[@{@"com.instagram.sharedSticker.backgroundImage" : backgroundImage,
-                                       @"com.instagram.sharedSticker.contentURL" : attributionURL}];
-        if (@available(iOS 10.0, *)) {
-            NSDictionary *pasteboardOptions = @{UIPasteboardOptionExpirationDate : [[NSDate date] dateByAddingTimeInterval:60 * 5]};
-            // This call is iOS 10+, can use 'setItems' depending on what versions you support
-            [[UIPasteboard generalPasteboard] setItems:pasteboardItems options:pasteboardOptions];
-        } else {
-            [[UIPasteboard generalPasteboard] setItems:pasteboardItems];
-        }
-        if (@available(iOS 10.0, *)) {
-            [[UIApplication sharedApplication] openURL:urlScheme options:@{} completionHandler:nil];
-        } else {
-            [[UIApplication sharedApplication] openURL:urlScheme];
-        }
-        
-    } else {
-        NSString *errorMessage = @"Not installed";
-        NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(errorMessage, nil)};
-        NSError *error = [NSError errorWithDomain:@"com.rninstagramstoryshare" code:1 userInfo:userInfo];
-
-        failureCallback(error);
-    }
++ (BOOL)requiresMainQueueSetup {
+  return YES;
 }
 
-- (NSDictionary *)constantsToExport
-{
-    return @{ @"NOT_INSTALLED": @"Not installed",
-              @"INTERNAL_ERROR": @"Data conversion failed",
-              @"NO_BASE64_IMAGE": @"No base64 image",
-              @"INVALID_PARAMETER": @"Invalid parameter"
-              };
+RCT_EXPORT_METHOD(share: (NSDictionary *)options
+                resolve: (RCTPromiseResolveBlock)resolve
+                reject: (RCTPromiseRejectBlock)reject) {									
+
+	NSString *provider  						= [RCTConvert NSString:options[@"provider"]];
+	NSString *appID  								= [RCTConvert NSString:options[@"appId"]];
+	NSURL *backgroundImageParam  		= [RCTConvert NSURL:options[@"backgroundImage"]];
+	NSURL *backgroundVideoParam  		= [RCTConvert NSURL:options[@"backgroundVideo"]];
+	NSURL *stickerImageParam     		= [RCTConvert NSURL:options[@"stickerImage"]];
+	NSString *backgroundTopColor    = [RCTConvert NSString:options[@"backgroundTopColor"]];
+	NSString *backgroundBottomColor = [RCTConvert NSString:options[@"backgroundBottomColor"]];
+	NSString *contentURL            = [RCTConvert NSString:options[@"contentURL"]];
+
+
+	NSString *dataError    = @"data conversion failed";
+	NSDictionary *userInfo = @{NSLocalizedFailureReasonErrorKey: NSLocalizedString(dataError, nil)};
+	NSError *error         = [NSError errorWithDomain:@"com.rninstagramstoryshare" code:1 userInfo:userInfo];
+
+  NSData *backgroundImage = nil;
+	if (backgroundImageParam) {
+		backgroundImage = [NSData dataWithContentsOfURL:backgroundImageParam
+																						options:(NSDataReadingOptions)0
+																					 		error:&error];
+		if (!backgroundImage) {
+			reject(RCTErrorUnspecified, dataError, error);
+			return;
+		}
+		backgroundImage = UIImagePNGRepresentation([UIImage imageWithData:backgroundImage]);
+	}
+
+  NSData *backgroundVideo = nil;
+	if (backgroundVideoParam) {
+		backgroundVideo = [[NSFileManager defaultManager] contentsAtPath:backgroundVideoParam];
+		if (!backgroundVideo) {
+			reject(RCTErrorUnspecified, dataError, error);
+			return;
+		}
+	}
+
+  NSData *stickerImage = nil;
+	if (stickerImageParam) {
+		stickerImage = [NSData dataWithContentsOfURL:stickerImageParam
+																				 options:(NSDataReadingOptions)0
+																					 error:&error];
+		if (!stickerImage) {
+			reject(RCTErrorUnspecified, dataError, error);
+			return;
+		}
+		stickerImage = UIImagePNGRepresentation([UIImage imageWithData:stickerImage]);
+	}    
+
+	[self 			 provider: provider
+									appID: appID
+				backgroundImage: backgroundImage
+				backgroundVideo: backgroundVideo
+					 stickerImage: stickerImage
+		 backgroundTopColor: backgroundTopColor
+	backgroundBottomColor: backgroundBottomColor
+						 contentURL: contentURL
+								resolve: resolve
+								 reject: reject];
+}
+
+- (void) 				provider: (NSString *)provider
+									 appID: (NSString *)appID
+				 backgroundImage: (NSData *)backgroundImage
+				 backgroundVideo: (NSData *)backgroundVideo
+				    stickerImage: (NSData *)stickerImage
+			backgroundTopColor: (NSString *)backgroundTopColor
+	 backgroundBottomColor: (NSString *)backgroundBottomColor
+				      contentURL: (NSString *)contentURL
+				         resolve: (RCTPromiseResolveBlock)resolve
+				          reject: (RCTPromiseRejectBlock)reject {
+
+	NSString *url    = [NSString stringWithFormat: @"%@-stories://share", provider];
+	NSURL *urlScheme = [NSURL URLWithString: url];
+
+	NSDictionary *dictionary               = @{}; 
+	NSMutableDictionary *mutableDictionary = [dictionary mutableCopy];
+
+	if ([provider isEqualToString:@"facebook"]) {
+		if (appID) {
+			NSString *key          = [NSString stringWithFormat: @"com.%@.sharedSticker.appID", provider];
+			mutableDictionary[key] = appID;
+		}
+	}
+
+	if (backgroundImage) {
+		NSString *key          = [NSString stringWithFormat: @"com.%@.sharedSticker.backgroundImage", provider];
+		mutableDictionary[key] = backgroundImage;
+	}
+
+	if (backgroundVideo) {
+		NSString *key          = [NSString stringWithFormat: @"com.%@.sharedSticker.backgroundVideo", provider];
+		mutableDictionary[key] = backgroundVideo;
+	}
+
+	if (stickerImage) {
+		NSString *key          = [NSString stringWithFormat: @"com.%@.sharedSticker.stickerImage", provider];
+		mutableDictionary[key] = stickerImage;
+	}
+
+	if (backgroundTopColor) {
+		NSString *key          = [NSString stringWithFormat: @"com.%@.sharedSticker.backgroundTopColor", provider];
+		mutableDictionary[key] = backgroundTopColor;
+	}
+	
+	if (backgroundBottomColor) {
+		NSString *key          = [NSString stringWithFormat: @"com.%@.sharedSticker.backgroundBottomColor", provider];
+		mutableDictionary[key] = backgroundBottomColor;
+	}
+
+	if (contentURL) {
+		NSString *key          = [NSString stringWithFormat: @"com.%@.sharedSticker.contentURL", provider];
+		mutableDictionary[key] = contentURL;
+	}
+	
+	NSArray *pasteboardItems = @[mutableDictionary];
+
+	if (@available(iOS 10.0, *)) {
+		NSDictionary *pasteboardOptions = @{UIPasteboardOptionExpirationDate : [[NSDate date] dateByAddingTimeInterval:60 * 5]};
+		[[UIPasteboard generalPasteboard] setItems:pasteboardItems options:pasteboardOptions];
+		[[UIApplication sharedApplication] openURL:urlScheme options:@{} completionHandler:nil];
+	} else {
+		[[UIPasteboard generalPasteboard] setItems:pasteboardItems];
+		[[UIApplication sharedApplication] openURL:urlScheme];
+	}
 }
 
 @end
--- src/index.js
+++ src/index.js
@@ -1,21 +1,72 @@
-import { NativeModules } from 'react-native'
+import {
+  NativeModules,
+  Linking,
+} from 'react-native'
+const {RNInstagramStoryShare} = NativeModules
 
-const { RNInstagramStoryShare } = NativeModules
+const share = options => {
+  return new Promise((resolve, reject) => {
+    const {appId, provider, backgroundImage, backgroundVideo, stickerImage, backgroundTopColor, backgroundBottomColor, contentURL} = options
 
-/* const ERROR_TYPES = {
-  NOT_INSTALLED: RNInstagramStoryShare.NOT_INSTALLED,
-  INTERNAL_ERROR: RNInstagramStoryShare.INTERNAL_ERROR,
-  NO_BASE64_IMAGE: RNInstagramStoryShare.NO_BASE64_IMAGE,
-  INVALID_PARAMETER: RNInstagramStoryShare.INVALID_PARAMETER,
+    if (!appId && !provider && !backgroundImage && !backgroundVideo && !stickerImage && !backgroundTopColor && !backgroundBottomColor && !contentURL) {
+      return reject(new Error('all params are undefined'))
+    }
+
+    //TODO(ca): check if FacebookAppID is present in Info.plist
+    if (provider !== 'facebook' && provider !== 'instagram') {
+      return reject(new Error('provider: invalid param'))
+    }
+    
+    if (provider === 'facebook' && !appId) {
+      return reject(new Error('appId: missing param'))
+    }
+    
+    if (provider === 'facebook'  && typeof appId !== 'string') {
+      return reject(new Error('appId: invalid param'))
+    }
+
+    if (backgroundImage && backgroundVideo) {
+      return reject(new Error('it must be just one, backgroundImage or backgroundVideo'))
+    }
+
+    if (backgroundImage && (typeof backgroundImage !== 'string' || !(backgroundImage.includes('file://') || backgroundImage.includes('data:')))) {
+      return reject(new Error('backgroundImage: invalid param'))
+    }
+
+    if (backgroundVideo && (typeof backgroundVideo !== 'string' || !(backgroundVideo.includes('file://') || backgroundVideo.includes('data:')))) {
+      return reject(new Error('backgroundVideo: invalid param'))
+    }
+
+    if (stickerImage && (typeof stickerImage !== 'string' || !(stickerImage.includes('file://') || stickerImage.includes('data:')))) {
+      return reject(new Error('stickerImage: invalid param'))
+    }
+
+    if (backgroundTopColor && (typeof backgroundTopColor !== 'string' || !backgroundTopColor.includes('#'))) {
+      return reject(new Error('backgroundTopColor: invalid param'))
+    } 
+
+    if (backgroundBottomColor && (typeof backgroundBottomColor !== 'string' || !backgroundBottomColor.includes('#'))) {
+      return reject(new Error('backgroundBottomColor: invalid param'))
+    } 
+
+    if (contentURL && (typeof contentURL !== 'string' || !contentURL.includes('://'))) {
+      return reject(new Error('contentURL: invalid param'))
+    } 
+
+    Linking.canOpenURL('instagram-stories://')
+      .then(() => RNInstagramStoryShare.share({
+        appId,
+        provider,
+        backgroundImage, 
+        backgroundVideo, 
+        stickerImage, 
+        backgroundTopColor, 
+        backgroundBottomColor, 
+        contentURL,
+      }))
+      .then(() => resolve())
+      .catch(error => reject(error))
+  })
 }
 
-const shareToInstagramStory = options =>
-  new Promise((resolve, reject) =>
-    RNInstagramStoryShare.share(options, error => reject(error), resolve)
-  )
-
-module.exports = { share: shareToInstagramStory, ERROR_TYPES }
-module.exports.shareToInstagramStory = shareToInstagramStory
-module.exports.ERROR_TYPES = ERROR_TYPES */
-
-module.exports = RNInstagramStoryShare
+module.exports = {share}
